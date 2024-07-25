b1241bc05fcb8de4efdbdecaa6a40b922938c118
M.Sree Abiram <143892160+Abiram116@users.noreply.github.com>
2024-07-25 21:59:14 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sree Abiram Mandava on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
